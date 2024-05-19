7679ff45824f3910249974c832a6d7f3075fdc4f
Jose Miguel <josemiguel.sandoval20@gmail.com>
2024-05-19 00:23:21 -0400

    tailwind test

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss';
 import pkg from './package.json';
 
 export default {
@@ -21,6 +22,16 @@ export default {
     },
   ],
   plugins: [
+    postcss({
+      config: {
+        path: './postcss.config.js',
+      },
+      extensions: ['.css'],
+      minimize: true,
+      inject: {
+        insertAt: 'top',
+      },
+    }),
     peerDepsExternal(),
     resolve(),
     commonjs(),
